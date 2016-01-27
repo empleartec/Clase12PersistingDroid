cdf425a2f77950db9c1d9422cea85e738aa8e81f
Cristian Duarte <cduarte@Cristians-MacBook-Pro.local>
2016-01-27 17:50:04 -0300

    Freshly created project

---
+++ app/src/main/java/persistdroid/mobaires/com/persistingdroid/MainActivity.java
@@ -0,0 +1,13 @@
+package persistdroid.mobaires.com.persistingdroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
